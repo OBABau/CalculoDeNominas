9bc132106cfe6ad3559952dbda49168b9d7babb2
Luis Islas <lislas1919@gmail.com>
2024-04-03 03:25:27 -0700

    Update iniciado.php

--- iniciado.php
+++ iniciado.php
@@ -9,9 +9,10 @@ include('app/sesion.php');
     <link rel="stylesheet" href="https://pro.fontawesome.com/releases/v5.10.0/css/all.css">
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.4.1/dist/css/bootstrap.min.css"
         integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
-    <link rel="stylesheet" href="css/gwen.css">
+    
     
     <link rel="icon" type="image/x-icon" href="../img/img/logo.png">
+    <link rel="stylesheet" href="css/gwen.css">
     <title>Iniciado</title>
 </head>
 
