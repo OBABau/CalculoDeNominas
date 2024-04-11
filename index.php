a266e4b81dd6b1192b01b378885fdd14bdc920f0
OBABau <bautistabni@gmail.com>
2024-04-10 21:40:21 -0700

    Update index.php

--- index.php
+++ index.php
@@ -1,1 +1,162 @@
-<h1>HolaMundo</h1>
+<html>
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link
+        href="https://fonts.googleapis.com/css2?family=Poppins:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;1,700&display=swap"
+        rel="stylesheet">
+    <link rel="stylesheet" href="https://pro.fontawesome.com/releases/v5.10.0/css/all.css">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.4.1/dist/css/bootstrap.min.css"
+    integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
+
+    <link rel="stylesheet" href="css/gwen.css">
+
+    <link rel="icon" type="image/x-icon" href="img/img/logo.png">
+    <title>Index</title>
+</head>
+
+<body>
+    <nav class="navbar">
+        <div class="navbarHeader">
+            <div class="navbarTitulo">TFT</div>
+        </div>
+        
+        <div class="navbarCuerpo">          
+            <a class="enlaceNavbar sombraTexto1" href="view/Empresa/verPlanes.php"> &nbsp;Ver Planes<i class="fa fa-chevron-down"></i></a>
+            <a class="enlaceNavbar sombraTexto1" href="view/Empresa/registrarEmpresas.php"> &nbsp;Regístrate<i class="fa fa-chevron-down"></i></a>
+            <a class="enlaceNavbar sombraTexto1" href="view/login.php"> &nbsp;Iniciar Sesión<i class="fa fa-chevron-down"></i></a>
+            <a class="enlaceNavbar sombraTexto1" href="view/loginRegistroPersonal.php"> &nbsp;Registro personal<i class="fa fa-chevron-down"></i></a>
+        </div>
+
+    </nav>
+
+    <div class="cuerpo2">        
+        <div class="hero1 sombra p-4">
+            <h1 class="text-center font-weight-bold text-white mt-4 titulo11">TFT</h1>            
+            <h1 class="text-center mb-4 subtitulo22">La nueva plataforma encargada de administrar nóminas.</h1>                   
+      <div class="d-flex container-fluid mt-3 sombra2" lc-helper="background" style="height:50vh;background:url(img/img/busines3.jpg)  center / cover no-repeat;">
+    </div>
+    <div class="container p-5 bg1 sombra2" style="margin-top:-100px">
+        <div class="row">
+            <div class="col-md-4 text-center align-self-center">
+                <div class="lc-block border-end border-2 ">
+                    <div editable="rich">
+                        <p class="display-4 font-weight-bold">¿Quiénes somos?</p>
+                    </div>
+                </div><!-- /lc-block -->
+            </div><!-- /col -->
+            <div class="col-md-8">
+                <div class="lc-block ">
+                    <div editable="rich">
+                        <p class="display-4 subtitulo11"><br> Axis Development.</p>
+                    </div>
+                </div><!-- /lc-block -->
+            </div><!-- /col -->
+        </div>
+        <div class="row">
+            <div class="col-md-9 offset-md-1">
+                <div class="lc-block mt-5">
+                    <div editable="rich">
+                        <p class="lead"> Una empresa apasionada por la innovación y el desarrollo de soluciones tecnológicas. En el corazón de nuestra misión se encuentra el compromiso con la excelencia en el diseño y la implementación de software de vanguardia.&nbsp;</p>
+                        <p class="lead">En Axis Development, entendemos que cada proyecto es único. Nos destacamos por nuestro enfoque personalizado, colaborando estrechamente con nuestros clientes para comprender sus necesidades y ofrecer soluciones adaptadas. </p>
+                    </div>
+                </div><!-- /lc-block -->
+            </div><!-- /col -->
+        </div>
+    </div>
+</div>
+
+<div class="contenedorCarrousel mx-auto mt-4">
+    <h1 class="text-center font-weight-bold mt-4 titulo11">GESTOR DE NÓMINAS</h1>
+    <div id="carouselExampleIndicators" class="carousel slide" data-ride="carousel">
+        <ol class="carousel-indicators">
+            <li data-target="#carouselExampleIndicators" data-slide-to="0" class="active"></li>
+            <li data-target="#carouselExampleIndicators" data-slide-to="1"></li>
+            <li data-target="#carouselExampleIndicators" data-slide-to="2"></li>
+        </ol>
+        <div class="carousel-inner sombra">
+            <div class="carousel-item active">
+                <img class="d-block w-100" src="img/img/business.jpg" alt="First slide">
+            </div>
+            <div class="carousel-item">
+                <img class="d-block w-100" src="img/img/busines3.jpg" alt="Second slide">
+            </div>
+                <div class="carousel-item">
+                    <img class="d-block w-100" src="img/img/busines4.jpg" alt="Third slide">
+                </div>
+        </div>
+        <a class="carousel-control-prev" href="#carouselExampleIndicators" role="button" data-slide="prev">
+            <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+            <span class="sr-only">Previous</span>
+        </a>
+        <a class="carousel-control-next" href="#carouselExampleIndicators" role="button" data-slide="next">
+            <span class="carousel-control-next-icon" aria-hidden="true"></span>
+            <span class="sr-only">Next</span>
+        </a>
+    </div>
+</div>
+</div>
+        
+       
+<div class="hero1 mt-3 sombra2">               
+<div class="container py-5">
+	<div class="row mb-4 align-items-center flex-lg-row-reverse">
+		<div class="col-md-6 col-xl-7 mb-4 mb-lg-0 " style="">
+			<!-- requires glightbox, please flag the option in the picostrap customizer panel-->
+
+
+			<div class="lc-block position-relative"><img class="img-fluid rounded shadow" src="img/ind1.jpg">
+					<svg xmlns="http://www.w3.org/2000/svg" width="5em" height="5em" fill="currentColor" class="text-white" viewBox="0 0 16 16" lc-helper="svg-icon">
+						
+					</svg>
+				</a>
+			</div>
+		</div><!-- /col -->
+		<div class="col-md-6 col-xl-5">
+			<div class="lc-block mb-3">
+				<div editable="rich">
+					<h1 class="fw-bolder display-2 subtitulo11">¿Problemas con las nóminas?</h1>
+				</div>
+			</div><!-- /lc-block -->
+			<!-- /lc-block -->
+
+
+			<div class="lc-block mb-4">
+				<div editable="rich">
+
+					<p class="lead">No te preocupes más. Con nuestra plataforma tu empresa tendrá la facilidad de administrar y contabilizar las nóminas de tus empleados de manera sencilla</p>
+
+				</div>
+			</div>
+			<div class="lc-block">
+				<a class="btn btn-lg btn-light" href="view/Empresa/registrarEmpresas.php" role="button">Registrate</a>
+			</div><!-- /lc-block -->
+		</div><!-- /col -->
+	</div>
+</div>
+ 
+ 
+       
+    </div>            
+ 
+  
+
+      <br>
+    <div class="footer">
+        <div class="mensajeFooter">            
+            No nos hacemos responsables por el mal uso de la información aquí presentada.
+            <br>
+            2023 &copy; TFT            
+        </div>
+    </div>
+
+    <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/popper.js@1.14.7/dist/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.3.1/dist/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
+
+    
+</body>
+</html>
